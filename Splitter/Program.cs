1c2f4ca44c6a9d9716ba5ffd58d7c33876356d5d
João Margatho <joao@mgotecnologia.com.br>
2021-11-02 13:41:22 -0300

    Refat

--- Splitter/Program.cs
+++ Splitter/Program.cs
@@ -9,54 +9,25 @@ namespace Splitter
     {
         static void Main(string[] args)
         {
-            var qtdLinhas = 1000;
+            var qtdLinhas = 100;
             string[] lines = System.IO.File.ReadAllLines(@"fonte.csv");
-
             int cont = 0;
             int page = 1;
-            List<string> aux = new List<string>();
+            string fileName = page + "_" + System.IO.Path.GetRandomFileName()  + ".csv";
             foreach (string line in lines)
             {
-                aux.Add(line);
                 if (cont == qtdLinhas)
                 {
-                    
-                    cont = 0;
-                    string fileName = page + "_" + System.IO.Path.GetRandomFileName()  + ".csv";
-                    var pathString = System.IO.Path.Combine("files", fileName);
-                    
-                    using (StreamWriter writer = new StreamWriter(pathString, true))
-                    {
-                        foreach(string linha in aux)
-                        {
-                            writer.WriteLine(linha);
-                            
-                        }
-                    }
-                    
-                    aux = new List<string>(); 
                     page++;
+                    fileName = page + "_" + System.IO.Path.GetRandomFileName() + ".csv";
+                    cont = 0;
                 }
-                cont++;
-            }
-            if(cont != 0){
-                string fileName = page + "_" + System.IO.Path.GetRandomFileName()  + ".csv";
                 var pathString = System.IO.Path.Combine("files", fileName);
-                if (!System.IO.File.Exists(pathString))
-                {
-                    using (System.IO.FileStream fs = System.IO.File.Create(pathString))
-                    {
-
-                    }
-                }
                 using (StreamWriter writer = new StreamWriter(pathString, true))
                 {
-                    foreach (string linha in aux)
-                    {
-                        writer.WriteLine(linha);
-
-                    }
+                    writer.WriteLine(line);
                 }
+                cont++;
             }
         }
     }
